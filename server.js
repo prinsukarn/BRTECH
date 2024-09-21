7cc708e7cf0d428591cc7d605bcd282b00d03026
pratiksha karn <karnprtksh11@gmail.com>
2024-09-21 14:02:23 +1000

    feat: update package.json and server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const contactRoute = require("./route/contactRoute");
 
@@ -13,6 +14,14 @@ app.use(cors());
 
 // Routes
 app.use("/", contactRoute);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// The "catchall" handler: for any request that doesn't match API routes, serve the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 // Port and Server Setup
 const port = process.env.PORT || 5000;
